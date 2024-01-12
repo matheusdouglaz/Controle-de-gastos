c8e9d457f452578f198549a8ee3fe5e6d5753585
Matheus Douglas Alves Pinto <matheus.pinto@mds.gov.br>
2024-01-12 17:23:05 -0300

    Primeiro commit

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAGyFnxDYC9jR-tk0MNOEPOwLoC9Euw6Jc",
+    authDomain: "controle-de-gastos-6b1d3.firebaseapp.com",
+    projectId: "controle-de-gastos-6b1d3",
+    storageBucket: "controle-de-gastos-6b1d3.appspot.com",
+    messagingSenderId: "818424912354",
+    appId: "1:818424912354:web:38aa2fdf47e06fd73f81bb"
+};
+firebase.initializeApp(firebaseConfig);
